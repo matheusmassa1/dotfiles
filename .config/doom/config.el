6b076cf36a0261814229765dcfe802294ea66529
Matheus Massa <matheusmassa@protonmail.com>
2024-08-10 20:23:48 -0300

    fix: typos

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -115,4 +115,4 @@
               ("<tab>" . 'copilot-accept-completion)
               ("TAB" . 'copilot-accept-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word))
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
